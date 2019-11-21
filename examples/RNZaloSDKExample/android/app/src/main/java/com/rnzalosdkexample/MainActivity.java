29f8dc4af6057f30c61d15c87da884408fd77ab4
Khoa Tran <khoatran.geek@gmail.com>
2019-11-21 18:50:55 +0700

    Setup Android for example app

--- examples/RNZaloSDKExample/android/app/src/main/java/com/rnzalosdkexample/MainActivity.java
+++ examples/RNZaloSDKExample/android/app/src/main/java/com/rnzalosdkexample/MainActivity.java
@@ -2,6 +2,10 @@ package com.rnzalosdkexample;
 
 import com.facebook.react.ReactActivity;
 
+import android.content.Intent;
+
+import com.zing.zalo.zalosdk.oauth.ZaloSDK;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -12,4 +16,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "RNZaloSDKExample";
   }
+
+  @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ZaloSDK.Instance.onActivityResult(this, requestCode, resultCode, data);
+    }
 }
--- examples/RNZaloSDKExample/android/app/src/main/java/com/rnzalosdkexample/MainApplication.java
+++ examples/RNZaloSDKExample/android/app/src/main/java/com/rnzalosdkexample/MainApplication.java
@@ -2,38 +2,44 @@ package com.rnzalosdkexample;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.ZaloSdkPackage;
+import com.zing.zalo.zalosdk.oauth.ZaloSDKApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.security.MessageDigest;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -43,8 +49,28 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    ZaloSDKApplication.wrap(this);
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    // initializeFlipper(this); // Remove this line if you don't want Flipper
+    // enabled
+    try {
+      Log.d("Application hash: ", getApplicationHashKey(this));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static String getApplicationHashKey(Context ctx) throws Exception {
+    PackageInfo info = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
+    for (Signature signature : info.signatures) {
+      MessageDigest md = MessageDigest.getInstance("SHA");
+      md.update(signature.toByteArray());
+      String sig = Base64.encodeToString(md.digest(), Base64.DEFAULT).trim();
+      if (sig.trim().length() > 0) {
+        return sig;
+      }
+    }
+    return null;
   }
 
   /**
@@ -56,9 +82,9 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
+         * We use reflection here to pick up the class that initializes Flipper, since
+         * Flipper library is not available in release mode
+         */
         Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
         aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
       } catch (ClassNotFoundException e) {
